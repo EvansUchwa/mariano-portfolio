aeea24b7115b507216896c614d1909608a3eb779
EvansUchwa <johnsonevans686@gmail.com>
2024-12-17 02:46:14 +0100

    Font size updated,and some style stuff changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,21 @@ import Navbar from "@/uikits/nav";
 import Footer from "@/uikits/footer";
 import { MyParticles } from "@/components/particle";
 
-const helvetica1 = localFont({
-  src: "./fonts/HelveticaNeueBold.otf",
-  variable: "--HelveticaNeueBold",
+const font1 = localFont({
+  src: "./fonts/Montserrat-Bold.ttf",
+  variable: "--MontserratBold",
   weight: "900",
 });
 
-const helvetica2 = localFont({
-  src: "./fonts/HelveticaNeueLight.otf",
-  variable: "--HelveticaNeueLight",
+const font2 = localFont({
+  src: "./fonts/Montserrat-Light.ttf",
+  variable: "--MontserratLight",
   weight: "100",
 });
 
-const helvetica3 = localFont({
-  src: "./fonts/HelveticaNeueMedium.otf",
-  variable: "--HelveticaNeueMedium",
+const font3 = localFont({
+  src: "./fonts/Montserrat-Regular.ttf",
+  variable: "--MontserratRegular",
   weight: "100",
 });
 
@@ -36,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${helvetica1.variable} ${helvetica2.variable}  ${helvetica3.variable}`}
+        className={`${font1.variable} ${font2.variable}  ${font3.variable}`}
       >
         <Navbar />
         {children}
